97a8c2e2585739da1aa37e4928a797d21c33a89a
badraih <aaa@aAas-MacBook-Pro.local>
2019-02-19 17:03:04 +0300

    update :)))

--- src/App.js
+++ src/App.js
@@ -15,36 +15,38 @@ import './reset.css'
  -call the function from child.*/
  
 
- let uncheckedBookVal=true; //global vairable -->take true if item checked otherwise take false
+//  let uncheckedBookVal=true; //global vairable -->take true if item checked otherwise take false
+ let countVal=0;
 class App extends Component {
  // declare state 
   state= {
-    // checkedBook to store all checked books ✔
-    checkedBook:[],
     checkCount:0,
     // formData new book data input 
     formData:{
        title:"",
        image:"",
-       note:""
+       note:"",
+       checked: 'false'
     },
-    bookStyle:false,
     // book_list store all book data 
      book_list:[
       {
         title:"How to Change Your Mind",
         image:"https://images.gr-assets.com/books/1522586374l/36710811.jpg",
-        note:"I finished reading a hundred pages"
+        note:"I finished reading a hundred pages",
+        checked: 'false'
       }
       ,{
         title:"The Rise and Fall of the Dinosaurs",
        image:"https://images.gr-assets.com/books/1515529573l/35820369.jpg",
-       note:"I read 300 pages"
+       note:"I read 300 pages",
+       checked: 'false'
       }
       ,{
         title:"Harry Potter",
         image:"https://images.gr-assets.com/books/1474154022l/3.jpg",
-        note:"(Harry Potter #1)"
+        note:"(Harry Potter #1)",
+        checked: 'false'
       }
     ]
   }//end state
@@ -60,51 +62,51 @@ class App extends Component {
       book_list:copy
     });
 
-   this.SaveDataToLocalStorage(this.state.formData); // call function to add new data to local storage :)
-}
-
-// SaveDataToLocalStorage() function to add new book data inside local storage 
-SaveDataToLocalStorage(data)
-{ 
-    var a = [];
-    // Parse the serialized data back into an array of objects
-    a = JSON.parse(localStorage.getItem('book_list'));
-    // Push the new data (whether it be an object or anything else) onto the array
-    a.push(data);
-    // Re-serialize the array back into a string and store it in localStorage
-    localStorage.setItem('book_list', JSON.stringify(a));
-}
-
-RemoveDataFromLocalStorage(data){
-//   console.log("iam RemoveDataFromLocalStorage",data);
-
-  const CopyBookList= this.state.book_list.slice(0);//make copy of book_list array
-  // for loop to removed checked item from CopyBookList 
-  for(var i=0;i<CopyBookList.length;i++){
-     if(CopyBookList[i].title===data.title){
-      CopyBookList.splice(i,1);// used splice function to remove from array
-     } 
+  //  this.SaveDataToLocalStorage(this.state.formData); // call function to add new data to local storage :)
   }
-  // CopyBookList=this.state.book_list.filter(bookListData=>bookListData!== data);
-  // console.log(CopyBookList[0]);
-  // set CopyBookList after removed checked book (completed)
-  this.setState({
-     book_list:CopyBookList
-  });
-  console.log("I am copy of Book List ",CopyBookList);
-  // this.SaveDataToLocalStorage(CopyBookList);
-  // localStorage.setItem('book_list', JSON.stringify(this.state.book_list));
-}
+
+  // SaveDataToLocalStorage() function to add new book data inside local storage 
+  SaveDataToLocalStorage(data)
+  { 
+      var a = [];
+      // Parse the serialized data back into an array of objects
+      a = JSON.parse(localStorage.getItem('book_list'));
+      // Push the new data (whether it be an object or anything else) onto the array
+      a.push(data);
+      // Re-serialize the array back into a string and store it in localStorage
+      localStorage.setItem('book_list', JSON.stringify(a));
+  }
+
+  RemoveDataFromLocalStorage(data){
+  //   console.log("iam RemoveDataFromLocalStorage",data);
+
+    const CopyBookList= this.state.book_list.slice(0);//make copy of book_list array
+    // for loop to removed checked item from CopyBookList 
+    for(var i=0;i<CopyBookList.length;i++){
+      if(CopyBookList[i].title===data.title){
+        CopyBookList.splice(i,1);// used splice function to remove from array
+      } 
+    }
+    // CopyBookList=this.state.book_list.filter(bookListData=>bookListData!== data);
+    // console.log(CopyBookList[0]);
+    // set CopyBookList after removed checked book (completed)
+    this.setState({
+      book_list:CopyBookList
+    });
+    console.log("I am copy of Book List ",CopyBookList);
+    // this.SaveDataToLocalStorage(CopyBookList);
+    // localStorage.setItem('book_list', JSON.stringify(this.state.book_list));
+  }
 
 // componentDidMount() -> after the initial render
   componentDidMount(){
-    if(!localStorage.getItem('book_list')){
-      localStorage.setItem("book_list", JSON.stringify(this.state.book_list));}
+    // if(!localStorage.getItem('book_list')){
+    //   localStorage.setItem("book_list", JSON.stringify(this.state.book_list));}
       
-    localStorage.getItem('book_list') && this.setState({
-        book_list: JSON.parse(localStorage.getItem('book_list')),
-      });
-      this.clearBookChecked();
+    // localStorage.getItem('book_list') && this.setState({
+    //     book_list: JSON.parse(localStorage.getItem('book_list')),
+    //   });
+      // this.clearBookChecked();
   }
 
   getBookData= (event)=>{
@@ -126,101 +128,59 @@ RemoveDataFromLocalStorage(data){
   
 //call function when click on button clear checked item -->to delete only checked book(completed)
   clearBookChecked=()=>{
-  
     const copyStateBook= this.state.book_list.slice(0);//make copy of book_list
-    const copyCheckedBook =  this.state.checkedBook.slice(0);  //make copy of checkedBook
+    // const copyCheckedBook =  this.state.checkedBook.slice(0);  //make copy of checkedBook
     //  used foreach to loop throw copyStateBook and remove the checkBooks
-
-    copyStateBook.forEach( (book , index) =>  {
-      if( copyCheckedBook.map(book => book.title).indexOf(book.title) > -1 ) {
-        copyStateBook.splice(index,1);
-
-      }
-    });
-
-
-  
-    //setState -> CopyStateBook after remove checked book()
+    const updatedList = this.state.book_list.filter(book => {
+      console.log(book)
+      return book.checked === 'false'
+    })
+    console.log('list', updatedList)
     this.setState({
-       book_list:copyStateBook, 
-       checkedBook: [],
-       bookStyle:true
-    });
-
-    // console.log("\n\n\n\n" , this.state.checkedBook)
-
-   // console.log("i am copy  ", copyStateBook);
-    
-  localStorage.setItem("book_list", JSON.stringify(copyStateBook));  //set copyStateBook
-  
+      book_list: updatedList
+    })
+  // localStorage.setItem("book_list", JSON.stringify(CopyStateBook));  set copyStateBook
   }
 
-
-  changeBookStyle = (style) => {
-
+  checkBook = (index) => {
+    console.log(index)
+    const copy= this.state.book_list.slice(0);//make copy of book_list array
+    if (copy[index].checked === 'checked'){
+      copy[index].checked = "false"
+    } else {
+      copy[index].checked = "checked"
+    }
+    localStorage.setItem("book_list", JSON.stringify(copy));  //set copyStateBook
     this.setState({
-      text: style.text , 
-      color:style.color
+      book_list: copy
     })
   }
 
-  storeBookChecked = (data) => {
-    const dataTitle=data.title;
-    console.log("dataTitle ",dataTitle);
-
-     
-    const copy= this.state.checkedBook.slice(0);//make copy of book_list array
-
-    console.log(uncheckedBookVal);
-    // 
-    if(uncheckedBookVal===true){
-     copy.push(data);
-  
-     this.setState({
-       checkedBook:copy
-     });
-    }
-    
-    // if checked book ✔
-     else{
-       for(var i=0;i<this.state.checkedBook.length;i++){
-         console.log("checkedbook title",this.state.checkedBook[i].title);
-         console.log("data title"+data.title);
-
-         if((dataTitle === this.state.checkedBook[i].title)&& (dataTitle!==undefined)){
-            copy.splice(i,1);
-            console.log(i);
-         }
-       }
-
-       this.setState({
-         checkedBook:copy
-       });
- 
-     }
+  Count=(Count)=> {
+    countVal=Count;
   }
-    uncheckedBook=(flag)=>{
-      uncheckedBookVal=flag;//set the val of 
-    }
-    Count=(Count)=>{
-     
-    }
     
 
   render(){
     // store all books by use map 
     // send the value to Book class 
-   console.log("print only checkedBook: ",this.state.checkedBook);
-  //  console.log("BookList:",this.state.book_list);
+    console.log("print only checkedBook: ",this.state.checkedBook);
+    //  console.log("BookList:",this.state.book_list);
 
-    const listOfBook=this.state.book_list.map((book)=>{
-         return <Book title={book.title} image={book.image} note={book.note} storeBookChecked={this.storeBookChecked} uncheckedBook={this.uncheckedBook} getBookStyle={this.changeBookStyle} stateStyle={this.state.bookStyle}/> ;
+    const listOfBook=this.state.book_list.map((book, index)=>{
+         return <Book book={book} 
+                      index={index}
+                      checkBook={this.checkBook} 
+                      // uncheckedBook={this.uncheckedBook} 
+                      getBookStyle={this.changeBookStyle} 
+                      Count={this.Count} /> ;
     });
 
     return (
       <div id="data">
         <h1 className="bookListHeader">Books List</h1>
         <button id="clear" onClick={this.clearBookChecked}> Clear checked items </button>
+        <p>{countVal}</p>
         <label>Title:</label>
         <input type="text" name="title" onChange={this.getBookData} value={this.state.formData.title}/>
         <label>Image:</label>
--- src/Book.js
+++ src/Book.js
@@ -6,92 +6,33 @@ import './reset.css'
 // take the props of note and image and title 
 // create checkbox for each book to mark the book as complete
 // change style of mark book by used state 
-let bookStyle1=false;
 class Book extends Component{
- 
-    // state object take impty value of the text and the color
-
-    state= {
-        text:'',
-        color:'',
-        checkBook:[]
-     }
-
-    checkBookState=(flag)=>{
-     let bookState=flag;
-     return bookState;
-    }
-    
-     checkedBook=(stateBook)=>{
-     let copy={};
-  
-    copy['title']=this.props.title;
-    copy['image']=this.props.image;
-    copy['note']=this.props.note;
-
-      if(stateBook===true){
-        this.props.uncheckedBook(true);
-        this.props.storeBookChecked(copy);
-      }
-      else{
-        this.props.uncheckedBook(false);
-        this.props.storeBookChecked(copy);
-      }
-
-     }
-
-    selectBook =(event)=> {
-      //  event.preventDefault();
-        let stateBook;
-
-        if(event.target.checked===true) {
-            this.setState({
-                text:"line-through", // set line-throug on text of mark book
-                color:'#A9A9A9',
-            });
-
-            stateBook= this.checkBookState(true);
-            this.checkedBook(stateBook);
-          }//end checked true 
-
-        else{
-            this.setState({
-                text:"",
-                color:'#000000',
-
-            });
-            stateBook= this.checkBookState(false);
-            this.checkedBook(stateBook);
-        }
-    }
-componentWillMount(){
-  bookStyle1=this.props.stateStyle;
- if(bookStyle1===true){
-   this.setState({
-       text:"",
-       color:'#000000'
-   });
- }
-}
-
     render(){
-        
+
        return (
           <div>
-              <table id="Books">
+            <table id="Books">
               <tr>
-               <th>Title</th>
-               <th>Book</th>
-               <th>note</th>
+                <th>Title</th>
+                <th>Book</th>
+                <th>note</th>
               </tr>
-            <tr>
-           <td><h2 className="header" style={{textDecorationLine:this.state.text , color:this.state.color}}> <input  value="0" type="checkbox" onChange={this.selectBook}  />  {this.props.title}</h2></td>
-           <td><img className="imageBook" src={this.props.image}></img></td>
-           <td> <p className="text" style={{textDecorationLine:this.state.text, color:this.state.color}} >{this.props.note}</p> </td>
-          </tr>
-           </table>
+              <tr>
+                <td>
+                  <h2 className={`${this.props.book.checked} header`}> 
+                    <input  value="0" 
+                            type="checkbox" 
+                            checked={this.props.book.checked === "checked"? true : false}
+                            onChange={()=>this.props.checkBook(this.props.index)}  />  
+                            {this.props.book.title}
+                  </h2>
+                </td>
+                <td><img className="imageBook" src={this.props.book.image}></img></td>
+                <td> <p className={`${this.props.book.checked} text`} >{this.props.book.note}</p> </td>
+              </tr>
+            </table>
 
-           </div>
+          </div>
            
        );
    }
